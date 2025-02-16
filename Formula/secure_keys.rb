d3702dc3a5f19eb649fa5606c523eeda28ddc060
Derian Córdoba <74283575+DerianCordobaPerez@users.noreply.github.com>
2025-02-16 00:44:58 -0600

    Fix Homebrew formula (#5)
    ### Description
    
    This PR addressed the homebew issue introduced in #4 
    
    ### Medias (if needed)
    
    - None.
    
    ### Testplan
    
    - [ ] I tested this change on my local environment.
    - [ ] I ran the unit tests.
    
    ### Checklist
    
    - [ ] I added tests for this change.
    - [ ] I checked the code style and run the linter.
    - [x] I added necessary documentation (if applicable).

--- Formula/secure_keys.rb
+++ Formula/secure_keys.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../lib/version', __dir__)
 
-class Keys < Formula
-  desc ::Keys::DESCRIPTION
-  homepage ::Keys::HOMEPAGE_URI
-  url "#{::Keys::HOMEPAGE_URI}/archive/refs/tags/v#{::Keys::VERSION}.tar.gz"
+class SecureKeys < Formula
+  desc Keys::DESCRIPTION
+  homepage Keys::HOMEPAGE_URI
+  url "#{Keys::HOMEPAGE_URI}/archive/refs/tags/v#{Keys::VERSION}.tar.gz"
   sha256 '99e6c3898553fe7ca01e0dc817175b967ec4e881808e015c07c5a34e2bf42cee'
   license 'MIT'
 
