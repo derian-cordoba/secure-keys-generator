c406fd6876968e0af01c3ff21809daec04a8ffe7
DerianCordobaPerez <derianricardo451@gmail.com>
2025-02-23 20:26:34 -0600

    feat(fixtures): added `UIKit` app fixture to show how to use the `SecureKeys` framework using `Firebase`

---
+++ fixtures/UIKitFirebaseApp/UIKitApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Derian Córdoba on 23/2/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
