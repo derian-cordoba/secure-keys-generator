6d2eef9635e978a1639889ee918181ba5df3686f
DerianCordobaPerez <derianricardo451@gmail.com>
2025-02-24 23:15:27 -0600

    feat(lib): added a method to include the `SecureKeys.xcframework` in an xcodeproj

--- lib/core/globals/globals.rb
+++ lib/core/globals/globals.rb
@@ -34,6 +34,13 @@ module SecureKeys
                                       .eql?('true')
     end
 
+    # Returns the Xcode project path
+    # @return [String] Xcode project path
+    def xcodeproj_path
+      Core::Console::Argument::Handler.fetch(key: :xcodeproj,
+                                             default: Dir.glob('**/*.xcodeproj').first)
+    end
+
     # Returns the supported iOS platforms
     # @return [Array] supported iOS platforms
     def ios_platforms
--- lib/core/utils/swift/xcframework.rb
+++ lib/core/utils/swift/xcframework.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
+require 'xcodeproj'
 require_relative './swift'
 require_relative '../../globals/globals'
 require_relative '../../console/shell'
+require_relative '../../console/logger'
+require_relative '../../console/arguments/handler'
 
 module SecureKeys
   module Swift
@@ -13,12 +16,13 @@ module SecureKeys
         # Currently this is failling with the following error:
         # "library with the identifier 'ios-arm64' already exists."
         %w[Release].each do |configuration|
-          SecureKeys::Globals.ios_platforms.each do |platform|
+          Globals.ios_platforms.each do |platform|
             generate_key_modules(configuration:, platform:)
             generate_key_libraries(configuration:, platform: platform[:path])
           end
         end
         generate_key_xcframework
+        add_xcframework_to_xcodeproj_target_if_needed
       end
 
       private
@@ -76,6 +80,41 @@ module SecureKeys
             "-library #{BUILD_DIRECTORY}/#{configuration}-#{platform[:path]}/lib#{SWIFT_PACKAGE_NAME}.a"
           end.join(' ')
         end.join(' ')
+      end
+
+      # Add the XCFramework to the Xcode project target if needed
+      # @param target_name [String] The target name to add the XCFramework
+      def add_xcframework_to_xcodeproj_target_if_needed(target_name: nil)
+        target_name ||= Core::Console::Argument::Handler.fetch(key: :target)
+
+        return if target_name.to_s.empty?
+
+        xcodeproj = Xcodeproj::Project.open(SecureKeys::Globals.xcodeproj_path)
+        return Core::Console::Logger.error(message: "The xcodeproj #{xcodeproj} already have the #{XCFRAMEWORK_DIRECTORY}") if xcodeproj_has_secure_keys_xcframework?(xcodeproj:)
+
+        Core::Console::Logger.important(message: "Adding the XCFramework to the target '#{target_name}'")
+        xcodeproj_target = xcodeproj.targets.find { |target| target.name.eql?(target_name) }
+
+        Core::Console::Logger.crash!(message: "The target #{target_name} was not found") if xcodeproj_target.nil?
+
+        xcframework_ref = xcodeproj.frameworks_group.new_file(XCFRAMEWORK_DIRECTORY)
+        xcodeproj_target.frameworks_build_phase.add_file_reference(xcframework_ref)
+
+        xcodeproj.save
+      end
+
+      # Check if the Xcode project has the secure keys XCFramework
+      # @param xcodeproj [Xcodeproj::Project] The Xcode project
+      # @return [Bool] true if the Xcode project has the secure keys XCFramework
+      def xcodeproj_has_secure_keys_xcframework?(xcodeproj:)
+        xcodeproj.targets.each do |target|
+          target.frameworks_build_phase.files.each do |file|
+            next if file.file_ref.nil?
+
+            return true if file.file_ref.path.include?(XCFRAMEWORK_DIRECTORY)
+          end
+        end
+        false
       end
     end
   end
